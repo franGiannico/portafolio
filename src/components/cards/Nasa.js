dde610c6b48cde72a0c11357698a0be15cc60ab5
Francisco Javier Giannico Villalobos <fjgiannico@gmail.com>
2022-10-28 20:37:59 -0300

    volviendo a la versión del comp nasa que funcionaba

--- src/components/cards/Nasa.js
+++ src/components/cards/Nasa.js
@@ -2,17 +2,12 @@ import { string } from "prop-types"
 
 
 const Nasa = ( { date, title, media_type, explanation, url } ) => {
-    return (
-        <div className="s-radius-1 s-shadow-bottom background s-shadow-card-micro white-color s-pxy-2">
-            <h3>Astronomy Picture of the Day by NASA</h3>
-            <h4>{ title }</h4>
-            <p>Fecha:{date}</p>
-
-            {media_type === 'img' && (
-                <img src={ url } alt={ media_type }/>
-            )}
-
-            {media_type === 'video' && (
+    if (media_type === 'video') {
+        return (
+            <div className="s-radius-1 s-shadow-bottom background s-shadow-card-micro white-color s-pxy-2">
+                <h3>Astronomy Picture of the Day by NASA</h3>
+                <h4>{ title }</h4>
+                <p>Fecha:{date}</p>
                 <iframe width="960" height="540" src={ url } 
                     title="YouTube video player" 
                     frameBorder="0" 
@@ -20,12 +15,23 @@ const Nasa = ( { date, title, media_type, explanation, url } ) => {
                     allowFullScreen="" 
                     alt={ media_type }>                    
                 </iframe>
-            )}
-            
-            <p>{ explanation }</p>
-        </div>
-    )
+                <p>{ explanation }</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="s-radius-1 s-shadow-bottom background s-shadow-card-micro white-color s-pxy-2">
+                <h3>Astronomy Picture of the Day by NASA</h3>
+                <h4>{ title }</h4>
+                <p>Fecha:{date}</p>
+                <img src={ url } alt={ media_type }/>
+                <p>{ explanation }</p>
+            </div>
+        )
+    }
 }
+
+
 
 Nasa.propTypes = {
     title: string,
